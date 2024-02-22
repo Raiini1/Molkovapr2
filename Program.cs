e966d2aca0d6b28239db367ddba9e3e0e8cdb161
Dota2003 <dinik66779@naluzotan.com>
2024-02-22 09:41:32 +0300

    добавление комита

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR1Molkova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 32;
+            int y = 4;
+            Console.WriteLine($"Вывод: {x / y}");
+        }
+    }
+}
